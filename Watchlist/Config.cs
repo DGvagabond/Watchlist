42633c206898093ff8849a0f7e9aa97f0c2da8b6
TypicalIllusion <70919038+TypicalIllusion@users.noreply.github.com>
2020-10-30 19:06:44 -0400

    Update Config.cs

--- Watchlist/Config.cs
+++ Watchlist/Config.cs
@@ -9,7 +9,7 @@ namespace Watchlist
         public bool IsEnabled { get; set; } = true;
         public bool Debug { get; set; }
         public string FolderPath { get; set; }= Path.Combine(Paths.Plugins, "Watchlist");
-        public int PingRoleId { get; set; } = 0;
-        public int LogChannel { get; set; } = 0;
+        public long PingRoleId { get; set; } = 0;
+        public long LogChannel { get; set; } = 0;
     }
 }
